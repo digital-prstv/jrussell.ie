1d1061a5b2c509051dff31e1a2e3de1c151823bb
Jeremiah Russell <jrussell@jerus.ie>
2021-11-03 09:03:11 +0000

    chore(iac): control of jrussell.ie move control of jrussell.ie to aws route 53

--- iac/20-network/resources.tf
+++ iac/20-network/resources.tf
@@ -15,6 +15,10 @@ resource "aws_acm_certificate" "domain_certificate" {
 
 resource "aws_route53_zone" "domain" {
   name = local.top_domain_name
+
+  tags = {
+    project = "about-me"
+  }
 }
 
 resource "aws_route53_record" "cert_validation_record" {
@@ -39,3 +43,31 @@ resource "aws_acm_certificate_validation" "domain_certificate_validation" {
   validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
 }
 
+resource "aws_route53_record" "google-mx" {
+  zone_id = aws_route53_zone.domain.zone_id
+  name    = ""
+  type    = "MX"
+  ttl     = "3600"
+
+  weighted_routing_policy {
+    weight = 10
+  }
+
+  set_identifier = "gmx1"
+  records = [
+    "1 aspmx.l.google.com",
+    "5 alt1.aspmx.l.google.com",
+    "5 alt2.aspmx.l.google.com",
+    "10 aspmx2.googlemail.com",
+    "10 aspmx3.googlemail.com"
+  ]
+}
+
+resource "aws_route53_record" "google-txt" {
+  zone_id = aws_route53_zone.domain.zone_id
+  name    = ""
+  type    = "TXT"
+  ttl     = "14400"
+
+  records = ["v=spf1 include:_spf.google.com ~all"]
+}
