a97372394c22cdae1adfe9fb3a23448d772762f7
Jeremiah Russell <jrussell@jerus.ie>
2021-11-03 17:51:05 +0000

    fix(iac): Remove encryption

--- iac/10-static/resources.tf
+++ iac/10-static/resources.tf
@@ -65,13 +65,13 @@ resource "aws_s3_bucket" "www_site" {
     target_prefix = "log/${local.site_name}/"
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       sse_algorithm = "AES256"
+  #     }
+  #   }
+  # }
 
   versioning {
     enabled = true
