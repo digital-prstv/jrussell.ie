70c42dac2087db769d0384647884fd6e9efe741d
Jeremiah Russell <jrussell@jerus.ie>
2021-11-12 11:30:36 +0000

    feat(iac): :sparkles: Build edge lambda to write security headers

---
+++ iac/22-sec-headers/outputs.tf
@@ -0,0 +1,7 @@
+output "lambda_arn" {
+  value = aws_lambda_function.lambda.arn
+}
+
+output "lambda_version" {
+  value = aws_lambda_function.lambda.version
+}
---
+++ iac/22-sec-headers/resources.tf
@@ -0,0 +1,40 @@
+locals {
+  name               = "www-jrussell-ie-sec-headers"
+  lambda_description = "Edge Lambda to apply security Headers for www.jrussell.ie"
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = local.name
+  description   = local.lambda_description
+  role          = aws_iam_role.role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs12.x"
+  filename      = "index.js"
+  publish       = true
+
+  source_code_hash = filebase64sha256("index.js")
+
+}
+
+resource "aws_iam_role" "role" {
+  name = local.name
+
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
+
+
+
+}
